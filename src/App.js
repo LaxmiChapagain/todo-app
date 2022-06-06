d0b39a89997e634da8c0cec05ba34eb0ec800012
Laxmi Chapagain <laxmichapagain1996@gmail.com>
2022-06-06 14:47:18 +0545

    Todo App using useContext and useReducer React hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./styles.css";
+import { reducer } from './reducers/todo.reducer';
+import { actions } from './reducers/todo.reducer';
 
-function App() {
+const initialState = {
+  todoList: []
+};
+
+
+const TodoListContext = React.createContext();
+const Provider = ({ children }) => {
+const [state, dispatch] = React.useReducer(reducer, initialState);
+const value = {
+
+    todoList: state.todoList,
+    addTodoItem: (todoItemLabel) => {
+      dispatch({ type: actions.ADD_TODO_ITEM, todoItemLabel });
+    },
+    removeTodoItem: (todoItemId) => {
+      dispatch({ type: actions.REMOVE_TODO_ITEM, todoItemId });
+    },
+    markAsCompleted: (todoItemId) => {
+      dispatch({ type: actions.TOGGLE_COMPLETED, todoItemId });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoListContext.Provider value={value}>
+      {children}
+    </TodoListContext.Provider>
+  );
+};
+
+const AddTodo = () => {
+  const [inputValue, setInputValue] = React.useState("");
+  const { addTodoItem } = React.useContext(TodoListContext);
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        placeholder={"Type and add todo item"}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          addTodoItem(inputValue);
+          setInputValue("");
+        }}
+      >
+        Add
+      </button>
+    </>
+  );
+};
+
+const TodoList = () => {
+  const { todoList, removeTodoItem, markAsCompleted } = React.useContext( TodoListContext);
+  return (
+    <ul>
+      {todoList.map((todoItem) => (
+        <li className={`todoItem ${todoItem.completed ? "completed" : ""}`} key={todoItem.id}
+         onClick={() => markAsCompleted(todoItem.id)}>
+          {todoItem.label}
+          <button className="delete" onClick={() => removeTodoItem(todoItem.id)} >
+            X
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default function App() {
+  return (
+    <Provider>
+      <AddTodo />
+      <TodoList />
+    </Provider>
   );
 }
 
-export default App;
+ 
+
+
